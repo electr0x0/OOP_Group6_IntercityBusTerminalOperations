49e5f2c37fc2f47658d08e04855f9b2fc6073481
samZero-0 <147060416+samZero-0@users.noreply.github.com>
2023-11-18 01:44:53 +0600

    addedPassenger

--- src/com/busterminal/model/Passenger.java
+++ src/com/busterminal/model/Passenger.java
@@ -10,4 +10,50 @@ package com.busterminal.model;
  */
 public class Passenger {
     
+    private String name,email,seatNumber;
+   private  int contactNum;
+
+    public Passenger(String name, String email, String seatNumber, int contactNum) {
+        this.name = name;
+        this.email = email;
+        this.seatNumber = seatNumber;
+        this.contactNum = contactNum;
+    }
+
+    public Passenger() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public int getContactNum() {
+        return contactNum;
+    }
+
+    public void setContactNum(int contactNum) {
+        this.contactNum = contactNum;
+    }
+}
+
 }
