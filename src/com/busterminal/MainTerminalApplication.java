d96ecdb828b05876e148e667f7357ca6decec9d6
Md Maruf <2221901@iub.edu.bd>
2023-11-26 22:34:43 +0600

    cv

--- src/com/busterminal/MainTerminalApplication.java
+++ src/com/busterminal/MainTerminalApplication.java
@@ -15,10 +15,10 @@ public class MainTerminalApplication extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/MaintenanceStaff/AddParts.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/Addministrator/ShowResignLetter.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/Addministrator/Feadback.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/Addministrator/AdminDashbord.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/Addministrator/AdminDashbord.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/Addministrator/Home.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/MaintenanceStaff/AddParts.fxml"));
-       // Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/MaintenanceStaff/MaintenanceStaffDashbord.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("/com/busterminal/views/MaintenanceStaff/MaintenanceStaffDashbord.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
